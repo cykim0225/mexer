93588cecc0d79ebc0e2c47438f150c9bc35c91a3
Yeongho (Chris) Kim <yck0225@gmail.com>
2020-11-01 13:22:47 -0800

    Added location inputbox

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,1 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {View, TextInput, Text, StyleSheet} from 'react-native';
+
+const Location = () => {
+  const [location, setLocation] = useState(false);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.input}>Enter a location</Text>
+      <TextInput
+        style={styles.inputbox}
+        onSubmitEditing={() => setLocation(true)}
+      />
+    </View>
+  );
+};
+
+export default Location;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    color: 'rgb(176, 176, 176)',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  inputbox: {
+    width: '70%',
+    height: 50,
+    borderColor: 'gray',
+    backgroundColor: 'rgb(210, 210, 210)',
+    borderWidth: 3,
+    borderRadius: 10,
+    marginBottom: 200,
+  },
+});
